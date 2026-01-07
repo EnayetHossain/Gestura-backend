700629a90eba1dd742ebd84ce2aaf90fe2dc49f9
EnayetHossain <enayethossasin135@gmail.com>
2026-01-07 15:08:00 +0600

    changed some routes

--- src/routes/installer.route.ts
+++ src/routes/installer.route.ts
@@ -1,11 +1,11 @@
-import { downloadInstaller, getLatestVersion, uploadUpdatedInstaller } from "@controllers/installer.controller"
+import { getDownloadURL, getLatestVersion, uploadUpdatedInstaller } from "@controllers/installer.controller"
 import upload from "@middlewares/multer.middleware"
 import { Router } from "express"
 
 const router = Router()
 
-router.route("/").get(getLatestVersion)
+router.route("/version").get(getLatestVersion)
 router.route("/upload").post(upload.single("installer"), uploadUpdatedInstaller)
-router.route("/download").get(downloadInstaller)
+router.route("/download").get(getDownloadURL)
 
 export default router;
